6bc10cf0a990a5e9b1b04962acab6b8ad810faf4
Ryley <jonesryley07@gmail.com>
2024-01-19 18:18:45 +0000

    Forgot to add the config.lua. So here it is

---
+++ config.lua
@@ -0,0 +1,31 @@
+-- config.lua
+
+Config = {}
+
+-- Loadout configuration
+Config.Loadout = {
+    Weapons = {
+        "WEAPON_PISTOL",
+        "WEAPON_STUNGUN",
+        -- Add more weapons as needed
+    },
+    AmmoCount = 50,
+}
+
+-- Police vehicle configuration
+Config.PoliceVehicles = {
+    "police",
+    "police2",
+    "police3",
+    "police4",
+    "policeb",
+    "policet",
+    "pbus",
+    "fbiranch",
+    "sheriff",
+    "sheriff2",
+    "riot",
+    "riot2",
+    "fbi",
+    -- Add more vehicle models as needed
+}
